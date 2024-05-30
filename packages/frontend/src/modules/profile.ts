8960aab549d23b3226d5376e9aa2ab251489cad8
nicolassutter <contact.sutter.nicolas@gmail.com>
2024-05-30 15:13:56 +0200

    feat: update profile bio

--- packages/frontend/src/modules/profile.ts
+++ packages/frontend/src/modules/profile.ts
@@ -1,8 +1,8 @@
 export const profile = {
   name: 'Sutter Nicolas',
-  bio: `I am currently a front & back-end web developer living in Strasbourg, France.
+  bio: `I am currently a technical supervisor and a full-stack developer working at Alsacréations, living in Strasbourg, France.
 I love to discover new technologies (and the JS ecosystem changes every 2 minutes, so that's nice I guess 🙃).
 I also repair printers for my family as all developers do obviously, and I game on my free time.`,
-  email: 'contact.sutter.nicolas@gmail.com',
+  email: 'contact@sutter-nicolas.com',
   github: 'https://github.com/nicolassutter',
 }
