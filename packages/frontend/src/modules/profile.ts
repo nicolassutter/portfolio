bf24e8c8c2ea6c97f9a929dd27a27e29ed9e9b6b
nicolassutter <contact.sutter.nicolas@gmail.com>
2024-09-18 14:03:17 +0200

    feat(profile): add freelance sites urls

--- packages/frontend/src/modules/profile.ts
+++ packages/frontend/src/modules/profile.ts
@@ -1,8 +1,16 @@
 export const profile = {
   name: 'Sutter Nicolas',
-  bio: `I am currently a technical supervisor and a full-stack developer working at Alsacréations, living in Strasbourg, France.
+  bio: `I am currently a freelance full-stack developer living in Strasbourg, France.
 I love to discover new technologies (and the JS ecosystem changes every 2 minutes, so that's nice I guess 🙃).
-I also repair printers for my family as all developers do obviously, and I game on my free time.`,
+I also repair printers for my family as all developers do obviously, and I game on my free time.
+
+On a more serious note, I am passionate about web development and I am always looking for new challenges.
+
+I am looking for missions mostly based on React, Vue.js or SolidJS (❤️), Node.js and TypeScript.
+I believe to be a good fit for a team that values quality, performance and maintainability; especially with all the JS based tech coming out these days.`,
   email: 'contact@sutter-nicolas.com',
   github: 'https://github.com/nicolassutter',
+  malt: 'https://www.malt.fr/profile/nicolassutter1',
+  codeur: 'https://www.codeur.com/-sutternicolas',
+  linkedin: 'https://www.linkedin.com/in/nicolas-sutter-abb18b188/',
 }
