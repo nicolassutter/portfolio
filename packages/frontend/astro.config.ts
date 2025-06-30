753ae9e56dd19880ffc7ae2279bb31eb38da771d
nicolassutter <contact.sutter.nicolas@gmail.com>
2025-06-30 15:31:45 +0200

    feat: add sveltia cms

--- packages/frontend/astro.config.ts
+++ packages/frontend/astro.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'astro/config'
 import url from 'node:url'
 import solidJs from '@astrojs/solid-js'
 import UnoCSS from 'unocss/astro'
+import decapCmsOauth from 'astro-decap-cms-oauth'
+
+import vercel from '@astrojs/vercel'
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+
+// ENV
+// OAUTH_GITHUB_CLIENT_ID=
+// OAUTH_GITHUB_CLIENT_SECRET=
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +19,11 @@ export default defineConfig({
       injectReset: true,
     }),
     solidJs(),
+    decapCmsOauth({
+      decapCMSSrcUrl: 'https://unpkg.com/@sveltia/cms/dist/sveltia-cms.js',
+    }),
   ],
+
   vite: {
     resolve: {
       alias: {
@@ -21,9 +32,12 @@ export default defineConfig({
     },
     plugins: [],
   },
+
   markdown: {
     shikiConfig: {
       theme: 'catppuccin-macchiato',
     },
   },
+
+  adapter: vercel(),
 })
