05bf71d9e3a9796432226ae3349fe1af8178b959
nicolassutter <contact.sutter.nicolas@gmail.com>
2025-06-30 16:21:46 +0200

    chore: add blog collections to decap cms

--- packages/frontend/types/types.ts
+++ packages/frontend/types/types.ts
@@ -1,8 +1,10 @@
 import { z } from 'astro:content'
 
+const maybeDate = z.string().datetime().optional()
+
 export const articleSchema = z.object({
   title: z.string(),
-  publishedAt: z.string().optional(),
+  publishedAt: maybeDate,
   views: z.number().optional(),
   tags: z.array(z.string()).optional(),
   category: z.string().optional(),
@@ -10,10 +12,10 @@ export const articleSchema = z.object({
   thumbnail: z.string(),
   url: z.string().optional(),
   id: z.union([z.number(), z.string()]),
-  date_created: z.string().optional(),
+  date_created: maybeDate,
   content: z.string().optional(),
   status: z.enum(['published', 'draft']).optional(),
-  date_updated: z.string().optional(),
+  date_updated: maybeDate,
   order: z.number(),
 })
 
